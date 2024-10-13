0aea2ac7438ec806bd639271ca0fa5a2d2e26635
Pornchanok Lerdsuppamongkon <pornchanok.ler@ku.th>
2024-10-13 14:48:30 +0700

    edit mycard & workflow file

--- src/MyCard.jsx
+++ src/MyCard.jsx
@@ -26,7 +26,8 @@ export default function MyCard(props) {
     </Typography>
   </CardContent>
   <CardActions>
-    <Button size="small">Learn More</Button>
+  <Button size="small">xxx</Button >
+   <Button size="small">Learn More</Button >
   </CardActions>
 </Card>
 
